4ab5903022c9e29a0f49d420896a9d598908f7b6
aaronmyatt <aaronmyatt@gmail.com>
2022-08-09 09:41:24 +0800

    4 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './style.css';
+import 'gridstack/dist/gridstack.min.css';
+import { GridStack } from 'gridstack';
+
+const grid = GridStack.init({ disableOneColumnMode: true });
+
+grid.addWidget({
+  w: 6,
+  h: 6,
+  content: document.querySelector('#title')!.innerHTML,
+});
+grid.addWidget({
+  w: 6,
+  h: 6,
+  content: document.querySelector('#example')!.innerHTML,
+});
+grid.addWidget({
+  w: 6,
+  h: 6,
+  content: document.querySelector('#about')!.innerHTML,
+});
+grid.addWidget({
+  w: 6,
+  h: 6,
+  content: document.querySelector('#contact')!.innerHTML,
+});
