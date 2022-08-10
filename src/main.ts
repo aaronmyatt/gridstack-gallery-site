f41dbdfabacf55c5328f7539e75000d05fc6ea4d
aaronmyatt <aaronmyatt@gmail.com>
2022-08-10 22:23:10 +0800

    update GSG version

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import './style.css';
 import 'gridstack/dist/gridstack.min.css';
 import { GridStack } from 'gridstack';
+import GridStackGallery from 'gridstack-gallery';
 
 const grid = GridStack.init({ disableOneColumnMode: true });
+
+const waitForSelector = setInterval(function () {
+  console.log(document.querySelectorAll('.app .gsg').length);
+  if (document.querySelectorAll('.app .gsg').length) {
+    // GridStackGallery();
+    clearInterval(waitForSelector);
+  }
+}, 100);
+
+declare global {
+  interface Window {
+    gsg: object;
+  }
+}
+
+grid.batchUpdate();
 
 grid.addWidget({
   w: 6,
@@ -24,3 +41,5 @@ grid.addWidget({
   h: 6,
   content: document.querySelector('#contact')!.innerHTML,
 });
+
+grid.commit();
